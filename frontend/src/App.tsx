373fa6925554f5d30fd3465f3bb17a7dc2beb44c
raspberry-pi <raspberry-pi@kakao.com>
2023-05-16 00:47:17 +0900

    Intial frontend commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import { UserAuth } from "components/UserAuth";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Route exact path="/" component={UserAuth} />
-      <Route path="/ranking" component={UserAuth} />
+    <div className="h-screen py-16 bg-gradient-to-br from-sky-50 to-gray-200 grid place-content-center">
+      <div className="bg-white p-6 rounded-md">
+        <div className="text-2xl font-bold">VodkaPay</div>
+        <div className="flex flex-col gap-4 pt-6">
+          <button className="border-2 rounded-lg text-xl py-4 px-12 text-center">New User</button>
+          <button className="border-2 rounded-lg text-xl py-4 px-12 text-center">Pay</button>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Root from './Root';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>
 );
 
